22a5e68364455363ec757bd8ca78987219a167d8
Andy Fragen <andy@thefragens.com>
2023-02-09 13:53:06 -0800

    initial commit

---
+++ plugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Dependencies API Example.
+ *
+ * Plugin Name:       Dependencies API Example
+ * Plugin URI:        https://github.com/afragen/dependencies-api-example
+ * Description:       Add plugin card data for non-dot org dependencies.
+ * Version:           0.1.0
+ * Author:            Andy Fragen
+ * License:           MIT
+ * Requires at least: 5.2
+ * Requires PHP:      7.1
+ * GitHub Plugin URI: https://github.com/afragen/dependencies-api-example
+ * Primary Branch:    main
+ * Requires Plugins: git-updater, gravityforms
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load the Composer autoloader.
+require __DIR__ . '/vendor/autoload.php';
+
+// Add the sites with REST endpoints that return plugins_api() data when passed `slug` query arg.
+add_filter(
+	'plugin_dependency_endpoints',
+	function () {
+		return array(
+			'https://git-updater.com/wp-json/git-updater/v1/plugins-api/',
+			'https://pub.thefragens.com/gravityforms.json',
+		);
+	}
+);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'afragen/dependencies-api-example',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'afragen/add-plugin-dependency-api' => array(
+            'pretty_version' => '0.4.5',
+            'version' => '0.4.5.0',
+            'reference' => 'c1400f9f132b8025d46c55aae2fe3c47c784908e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../afragen/add-plugin-dependency-api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'afragen/dependencies-api-example' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
